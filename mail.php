564e642f927e4e2aac9c84d370c3e08211d00f1f
prateek115 <prateekgour15@gmail.com>
2022-01-17 21:55:11 +0530

    Required changes in mail.php, index and thankyou.

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $email= $_POST['email'];
 $number= $_POST['number'];
 $area= $_POST['area'];
 $message= $_POST['subject'];
-$to = "youremail@mail.com";
+$to = "prateekgour15@gmail.com";
 $subject = "Mail From website";
 $txt ="FName = ". $name ."\r\n LName = " . $lname . "\r\n  Email = " . $email . "\r\n  Number = " . $number . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
